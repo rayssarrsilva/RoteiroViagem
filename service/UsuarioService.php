63b3446f82537f2bfb6b5891036d60e054c0961d
Rayssa <rayssarrsilva1@gmail.com>
2025-06-03 15:16:25 -0300

    usuarioService corrigido

--- service/UsuarioService.php
+++ service/UsuarioService.php
@@ -1,38 +1,5 @@
 <?php
-require_once __DIR__ . '/../dao/UsuarioDAO.php';
-
-class UsuarioService {
-    private $usuarioDAO;
-
-    public function __construct() {
-        $this->usuarioDAO = new UsuarioDAO();
-    }
-
-    public function listar() {
-        return $this->usuarioDAO->listarTodos();
-    }
-
-    public function buscar($id) {
-        return $this->usuarioDAO->buscarPorId($id);
-    }
-
-    public function criar($dados) {
-        return $this->usuarioDAO->inserir($dados['nome'], $dados['email']);
-    }
-
-    public function atualizar($id, $dados) {
-        return $this->usuarioDAO->atualizar($id, $dados['nome'], $dados['email']);
-    }
-
-    public function deletar($id) {
-        return $this->usuarioDAO->deletar($id);
-    }
-}
-?>
-
-controller/UsuarioController.php
-<?php
-require_once 'service/UsuarioService.php';
+require_once __DIR__ . '/../service/UsuarioService.php';
 
 class UsuarioController {
     private $service;
@@ -71,4 +38,3 @@ class UsuarioController {
         }
     }
 }
-?>
