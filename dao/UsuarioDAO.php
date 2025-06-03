a28a9b0d1aed6ad21752581faee3773d50d713b8
Rayssa <rayssarrsilva1@gmail.com>
2025-06-03 15:15:08 -0300

    usuarioDAO.php correção

--- dao/UsuarioDAO.php
+++ dao/UsuarioDAO.php
@@ -37,36 +37,3 @@ class UsuarioDAO {
     }
 }
 ?>
-
-service/UsuarioService.php
-<?php
-require_once 'dao/UsuarioDAO.php';
-
-class UsuarioService {
-    private $usuarioDAO;
-
-    public function __construct() {
-        $this->usuarioDAO = new UsuarioDAO();
-    }
-
-    public function listar() {
-        return $this->usuarioDAO->listarTodos();
-    }
-
-    public function buscar($id) {
-        return $this->usuarioDAO->buscarPorId($id);
-    }
-
-    public function criar($dados) {
-        return $this->usuarioDAO->inserir($dados['nome'], $dados['email']);
-    }
-
-    public function atualizar($id, $dados) {
-        return $this->usuarioDAO->atualizar($id, $dados['nome'], $dados['email']);
-    }
-
-    public function deletar($id) {
-        return $this->usuarioDAO->deletar($id);
-    }
-}
-?>
